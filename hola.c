2fd2232688c60be56ed0b5fc00eb3fe494d4d2e4
AnaPau17 <anapaulanavh@gmail.com>
2024-11-25 10:37:23 -0600

    Update hola.c

--- hola.c
+++ hola.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char **argv){ //argv es el arreglo y argc es el tamaño del arreclo 
     for(int i=1; i<3; i++){
-    printf("Hola %s\n", argv[i]);
+        printf("Hola %s\n", argv[i]);
     }
     return 0;
 }
