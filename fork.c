5d477f237e0a0a36dc44ac314817db2103b517d6
AnaPau17 <anapaulanavh@gmail.com>
2024-11-25 10:40:55 -0600

    Update fork.c

--- fork.c
+++ fork.c
@@ -6,6 +6,7 @@ int main(){
     int pid = fork();
     if(pid==0){
         printf("soy el proceso hijo a=%d\n",a);
+        sleep(20);
         return 0;
     }
     printf("soy el proceso padre a=%d\n",a);
